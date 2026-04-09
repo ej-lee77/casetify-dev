097345cbeb57903b77863ea9585809a78a7c394e
cjw6548 <cjw6548@gmail.com>
2026-04-09 18:51:05 +0900

    260408_05_스튜디오 섹션 작성

--- src/components/StudioInfoSection.jsx
+++ src/components/StudioInfoSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SectionTitle from './SectionTitle'
+import "./scss/StudioInfoSection.scss"
 import { Link } from 'react-router-dom'
 
 export default function StudioInfoSection() {
@@ -7,7 +8,9 @@ export default function StudioInfoSection() {
         <section className="studio-info">
             <SectionTitle title={"CASETiFY STUDIO"} subtitle={"전국 오프라인 매장에서 만나보세요!"} />
             <div className="inner">
-                <div className="img-box"></div>
+                <div className="img-box">
+                    <img src="./images/main-page/studio_section-img_pangyo.jpg" alt="" />
+                </div>
                 <div className="txt-box">
                     <h3>케이스티파이와 함께 나만의 스타일을 완성하세요</h3>
                     <ul className="stdio-feature">
