3ff1068de14d9bf1318670cd985493c2ab5307e3
cky0u0-ctrl <cky0u0@gmail.com>
2026-04-10 07:24:59 +0900

    cky0u0파일 복붙함

---
+++ src/components/BP/BP.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Swiper from './Swiper'
+
+export default function Bp() {
+    return (
+        <>
+            <div className="inner">
+
+
+
+
+            </div>
+
+
+        </>
+    )
+}
---
+++ src/components/BP/BpList.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+
+
+const Bpproduct = [
+    {
+        id: 1,
+        title: "Happiest KKOTKA in the world",
+        price: 29000,
+        image: "./images/KY/B/BpProduct001.png"
+    },
+
+    {
+        id: 2,
+        title: "Happiest KKOTKA in the world",
+        price: 29000,
+        image: "./images/KY/B/BpProduct002.png"
+    },
+
+    {
+        id: 3,
+        title: "Happiest KKOTKA in the world",
+        price: 29000,
+        image: "./images/KY/B/BpProduct003.png"
+    },
+
+
+    {
+        id: 4,
+        title: "Happiest KKOTKA in the world",
+        price: 29000,
+        image: "./images/KY/B/BpProduct004.png"
+    },
+
+    // { id: 5, title: "Happiest KKOTKA in the world", price: 29000, image: "../../public/images/KY/B/BpProduct005.png" },
+    // { id: 6, title: "Happiest KKOTKA in the world", price: 29000, image: "../../public/images/KY/B/BpProduct006.png" },
+    // { id: 7, title: "Happiest KKOTKA in the world", price: 29000, image: "../../public/images/KY/B/BpProduct007.png" },
+    // { id: 8, title: "Happiest KKOTKA in the world", price: 29000, image: "../../public/images/KY/B/BpProduct008.png" },
+    // { id: 9, title: "Happiest KKOTKA in the world", price: 29000, image: "../../public/images/KY/B/BpProduct009.png" },
+    // { id: 10, title: "Happiest KKOTKA in the world", price: 29000, image: "../../public/images/KY/B/BpProduct010.png" },
+    // { id: 11, title: "Happiest KKOTKA in the world", price: 29000, image: "../../public/images/KY/B/BpProduct011.png" },
+    // { id: 12, title: "Happiest KKOTKA in the world", price: 29000, image: "../../public/images/KY/B/BpProduct012.png" },
+    // { id: 13, title: "Happiest KKOTKA in the world", price: 29000, image: "../../public/images/KY/B/BpProduct013.png" },
+    // { id: 14, title: "Happiest KKOTKA in the world", price: 29000, image: "../../public/images/KY/B/BpProduct014.png" },
+    // { id: 15, title: "Happiest KKOTKA in the world", price: 29000, image: "../../public/images/KY/B/BpProduct015.png" },
+    // { id: 16, title: "Happiest KKOTKA in the world", price: 29000, image: "../../public/images/KY/B/BpProduct016.png" },
+];
+
+export default function BpList() {
+    // const [Bpproduct, setBpproduct] = useState(Bpproduct);
+    return (
+        <li className='product'>
+            {Bpproduct.map((item) => (
+                <ul className='right'>
+                    <li>
+                        <div>
+                            <img src={item.image} alt="" />
+                        </div>
+                        <div>
+                            <p>{item.title}</p>
+                            <p>{item.price}</p>
+                        </div>
+                    </li>
+                </ul>
+            ))}
+        </li>
+    )
+}
