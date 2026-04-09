3e4fb6ab6d96e753e8261bdaa66f1922cb2c4e7f
cjw6548 <cjw6548@gmail.com>
2026-04-09 19:15:50 +0900

    260408_05_스튜디오 섹션 이미지 위치 변경

--- src/components/StudioInfoSection.jsx
+++ src/components/StudioInfoSection.jsx
@@ -9,7 +9,7 @@ export default function StudioInfoSection() {
             <SectionTitle title={"CASETiFY STUDIO"} subtitle={"전국 오프라인 매장에서 만나보세요!"} />
             <div className="inner">
                 <div className="img-box">
-                    <img src="./images/main-page/studio_section-img_pangyo.jpg" alt="" />
+                    <img src="./images/main/section/studio/studio_pangyo.jpg" alt="" />
                 </div>
                 <div className="txt-box">
                     <h3>케이스티파이와 함께 나만의 스타일을 완성하세요</h3>
