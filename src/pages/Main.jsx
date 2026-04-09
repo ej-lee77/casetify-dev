18650f02304a081b85c55be6bd718d73d8261208
wldbs2334 <wldbs2334@gmail.com>
2026-04-09 18:42:32 +0900

    메인슬라이더

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,29 +1,18 @@
 import React from 'react'
-<<<<<<< HEAD
 import MainSlider from '../components/MainSlider'
-
-export default function Main() {
-  return (
-    <>
-      <MainSlider />
-      <div className="sub-page-wrap">
-        Main
-      </div>
-    </>
-=======
-import SectionTitle from '../components/SectionTitle'
-import { Link } from 'react-router-dom'
 import StudioInfoSection from '../components/StudioInfoSection'
 import QualityInfoSection from '../components/QualityInfoSection'
 import NewArrivalSection from '../components/NewArrivalSection'
 
 export default function Main() {
   return (
-    <div className="sub-page-wrap">
-      <NewArrivalSection />
-      <StudioInfoSection />
-      <QualityInfoSection />
-    </div>
->>>>>>> c7735dd3ed630f647ac4aa5df1a69518af3f3387
+    <>
+      <MainSlider />
+      <div className="sub-page-wrap">
+        <NewArrivalSection />
+        <StudioInfoSection />
+        <QualityInfoSection />
+      </div>
+    </>
   )
 }
