8afe1d823fa8b5e55a021b2cee0532b778facf05
cky0u0-ctrl <cky0u0@gmail.com>
2026-04-10 18:17:44 +0900

    BP.Custom 메인섹션업뎃

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,11 +4,16 @@ import { Link } from 'react-router-dom'
 import Studio from '../components/Studio'
 import Quality from '../components/Quality'
 import NewArrival from '../components/NewArrival'
+import BestProduct from '../components/main/BestProduct'
+import Custom from '../components/main/Custom'
+
 
 export default function Main() {
   return (
     <div className="sub-page-wrap">
+      <BestProduct />
       <NewArrival />
+      <Custom />
       <Studio />
       <Quality />
     </div>
