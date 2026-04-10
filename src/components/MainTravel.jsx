eea5504929b8e70d8d267a98afafacf16b67625d
youngseo-claire <claireyslahto@gmail.com>
2026-04-10 09:54:40 +0900

    travel section

--- src/components/MainTravel.jsx
+++ src/components/MainTravel.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
+
+import "./scss/maintravel.scss"
 
 export default function MainTravel() {
-    const handleEnter = (e) => {
-        e.target.play();
+    const handlePlay = () => {
+        const video = document.getElementById("travel-player");
+        video.style.display = "block";
+        video.play();
     };
-
-    const handleLeave = (e) => {
-        e.target.pause();
-        e.target.currentTime = 0;
-    };
-
     return (
-        <section className="video-section">
-            <video
-                muted
-                playsInline
-                onMouseEnter={handleEnter}
-                onMouseLeave={handleLeave}
-            >
-                <source src="./videos/travel.mp4" type="video/mp4" />
-            </video>
-        </section>);
+        <section className="travel">
+            <div className="travel-video">
+                <p><button className="travel-play-btn" onClick={handlePlay}>
+                    ▶
+                </button></p>
+                <img
+                    className="travel-bg"
+                    src="./images/main/travel.png"
+                    alt="바운스캐리어 동영상"
+                />
+                <video
+                    id="travel-player"
+                    className="travel-player"
+                    muted
+                    controls
+                >
+                    <source src="./video/main-travel.mp4" type="video/mp4" />
+                </video>
+                <div className="travel-text">
+                    <h2>Travel</h2>
+                    <p>발걸음마다 예술이 되는 나만의 움직이는 캔버스</p>
+                </div>
+            </div>
+        </section >
+    );
 }
