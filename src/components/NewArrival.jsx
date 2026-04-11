d650e459abbf764e3b887bbc42fb1e88c397ac39
cjw6548 <cjw6548@gmail.com>
2026-04-11 21:23:31 +0900

    260411_01_New Arrival 섹션 작업: 배열 데이터 map 적용 진행중

--- src/components/NewArrival.jsx
+++ src/components/NewArrival.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SectionTitle from './SectionTitle'
 import "./scss/NewArrival.scss"
 
 // new arrival 데이터
 const newArrivalData = [
-    { id: 1, icon: "./images/main/section/newArrival/charm-blue.png", bigImg: "./images/main/section/newArrival/nw-photo-02.jpg", text: "DENIM MANIFESTO", className: "denim", color: "#11419c" },
-    { id: 2, icon: "./images/main/section/newArrival/charm-yellow.png", bigImg: "./images/main/section/newArrival/nw-photo-04.jpg", text: "HI! FOREST", className: "yellow", color: "#d2b452" },
-    { id: 3, icon: "./images/main/section/newArrival/charm-red.png", bigImg: "./images/main/section/newArrival/nw-photo-01.jpg", text: "PILLOW CASE", className: "red", color: "#7F1D20" },
-    { id: 4, icon: "./images/main/section/newArrival/charm-pink.png", bigImg: "./images/main/section/newArrival/nw-photo-04.jpg", text: "SPRING IN BLOOM", className: "pink", color: "#EACAD6" },
+    { id: 1, icon: "./images/main/section/newArrival/charm-blue.png", bigImg: "./images/main/section/newArrival/nw-photo-01.jpg", text: "DENIM MANIFESTO", itemColor: "denim", color: "#11419c" },
+    { id: 2, icon: "./images/main/section/newArrival/charm-yellow.png", bigImg: "./images/main/section/newArrival/nw-photo-02.jpg", text: "HI! FOREST", itemColor: "yellow", color: "#d2b452" },
+    { id: 3, icon: "./images/main/section/newArrival/charm-red.png", bigImg: "./images/main/section/newArrival/nw-photo-03.jpg", text: "PILLOW CASE", itemColor: "red", color: "#7F1D20" },
+    { id: 4, icon: "./images/main/section/newArrival/charm-pink.png", bigImg: "./images/main/section/newArrival/nw-photo-04.jpg", text: "SPRING IN BLOOM", itemColor: "pink", color: "#EACAD6" },
 ]
 
 export default function NewArrival() {
+    // item 정보 파악 변수
+    const { activeTab, setActiveTab } = useState(1);
+
     return (
         <section className="new-arrival">
             <SectionTitle title={"New Arrival"} subtitle={"케이스티파이의 새로운 제품"} />
             <div className="inner">
                 <div className="new-arr-inner-wrap">
                     <ul className="new-arr-list">
-                        <li className="arr-item">
+                        {/* <li className="arr-item">
                             <img src="./images/main/section/newArrival/charm-blue.png" alt="img" />
                             <p>DENIM MANIFESTO</p>
                         </li>
@@ -29,7 +32,13 @@ export default function NewArrival() {
                             <p>PILLOW CASE</p></li>
                         <li className="arr-item">
                             <img src="./images/main/section/newArrival/charm-pink.png" alt="img" />
-                            <p>SPRING IN BLOOM</p></li>
+                            <p>SPRING IN BLOOM</p></li> */}
+                        {newArrivalData.map((arridata, id) => (
+                            <li key={id} className="arr-item">
+                                <img src={arridata.icon} alt={`icon_${arridata.itemColor}`} />
+                                <p>{arridata.text}</p>
+                            </li>
+                        ))}
                     </ul>
                     <div className="new-arr-img-box">
                         <img src="./images/main/section/newArrival/nw-photo-01.jpg" alt="" />
