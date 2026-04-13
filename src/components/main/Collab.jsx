2aff551ed2cb88d6881fae40cd23b0f4dba92c0c
wldbs2334 <wldbs2334@gmail.com>
2026-04-13 18:11:06 +0900

    콜라보 섹션 만들기시작~

---
+++ src/components/main/Collab.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+import "../scss/Collab.scss"
+
+
+export default function Collab() {
+    const slides = [
+        {
+            id: "1",
+            colImg: "./images/main/section/collab/collab-prod1.png",
+            title: "",
+            theme: ""
+        },
+        {
+            id: "2",
+            colImg: "./images/main/section/collab/collab-prod2.png",
+            title: "",
+            theme: ""
+        },
+        {
+            id: "3",
+            colImg: "./images/main/section/collab/collab-prod3.png",
+            title: "",
+            theme: ""
+        },
+        {
+            id: "4",
+            colImg: "./images/main/section/collab/collab-prod4.png",
+            title: "",
+            theme: ""
+        },
+        {
+            id: "5",
+            colImg: "./images/main/section/collab/collab-prod5.png",
+            title: "",
+            theme: ""
+        },
+        {
+            id: "6",
+            colImg: "./images/main/section/collab/collab-prod6.png",
+            title: "",
+            theme: ""
+        },
+        {
+            id: "7",
+            colImg: "./images/main/section/collab/collab-prod7.png",
+            title: "",
+            theme: ""
+        },
+        {
+            id: "8",
+            colImg: "./images/main/section/collab/collab-prod8.png",
+            title: "",
+            theme: ""
+        },
+        {
+            id: "9",
+            colImg: "./images/main/section/collab/collab-prod9.png",
+            title: "",
+            theme: ""
+        },
+        {
+            id: "10",
+            colImg: "./images/main/section/collab/collab-prod10.png",
+            title: "",
+            theme: ""
+        },
+        {
+            id: "11",
+            colImg: "./images/main/section/collab/collab-prod11.png",
+            title: "",
+            theme: ""
+        },
+        {
+            id: "12",
+            colImg: "./images/main/section/collab/collab-prod12.png",
+            title: "",
+            theme: ""
+        },
+    ]
+    return (
+        <section>
+            <div className="inner">
+
+            </div>
+        </section>
+    )
+}
