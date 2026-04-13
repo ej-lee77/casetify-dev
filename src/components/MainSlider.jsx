7d6c3a06a29ff264e8683f31d2b5dae1b9e2a05c
wldbs2334 <wldbs2334@gmail.com>
2026-04-13 09:25:59 +0900

    ms

--- src/components/MainSlider.jsx
+++ src/components/MainSlider.jsx
@@ -10,11 +10,34 @@ import "./scss/MainSlider.scss"
 
 export default function MainSlider() {
     const slides = [
-        { src: "./images/main-slider/main-slider1.png", alt: "main-slider1" },
-        { src: "./images/main-slider/main-slider2.png", alt: "main-slider2" },
-        { src: "./images/main-slider/main-slider3.png", alt: "main-slider3" },
-        { src: "./images/main-slider/main-slider4.png", alt: "main-slider4" },
-        { src: "./images/main-slider/main-slider5.png", alt: "main-slider5" },
+        {
+            src: "./images/main-slider/main-slider1.png", alt: "main-slider1",
+            titleImg: "./images/main-slider/main-slider-title1.png",
+            text: "나를 만난 모든 순간, 그 모든 여정에 함께."
+        },
+        {
+            src: "./images/main-slider/main-slider2.png", alt: "main-slider2",
+            titleImg: "./images/main-slider/main-slider-title2.webp",
+            text: "에스더 버니와 함께하는 즐거운 여정!"
+        },
+        {
+            src: "./images/main-slider/main-slider3.png", alt: "main-slider3",
+            titleImg: "./images/main-slider/main-slider-title3.png",
+            text: "Infinity In Bloom Collection",
+            sub: "천상의 리본과 끝없는 가능성이 만나는 곳"
+        },
+        {
+            src: "./images/main-slider/main-slider4.png", alt: "main-slider4",
+            titleImg: "./images/main-slider/main-slider-title4.png",
+            text: "완벽하게 당신다운,",
+            sub: " 세상에 없던 단 하나의 케이스."
+        },
+        {
+            src: "./images/main-slider/main-slider5.png", alt: "main-slider5",
+            titleImg: "./images/main-slider/main-slider-title5.png",
+            text: "단 하나의 마그네틱 스타, 단 하나의 독보적인 에딧.",
+            sub: "아일릿 원희의 감성으로 채운 Y2K"
+        },
     ]
     return (
         <div>
@@ -24,13 +47,19 @@ export default function MainSlider() {
                     delay: 5000,
                     disableOnInteraction: false
                 }}
-                navigation={true}
                 pagination={{ clickable: true }}
                 loop={true}
             >
                 {slides.map((slide, i) => (
                     <SwiperSlide key={i}>
                         <img src={slide.src} alt={slide.alt} />
+
+                        {/* 슬라이더 제목 */}
+                        <div className="slide-title">
+                            <img src={slide.titleImg} alt="title" className='title-img' />
+                            {slide.text && <h2>{slide.text}</h2>}
+                            {slide.sub && <p>{slide.sub}</p>}
+                        </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
