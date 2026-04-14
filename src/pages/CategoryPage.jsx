420666ef54c0a992e2a532af405f4d3128b9dc8b
youngseo-claire <claireyslahto@gmail.com>
2026-04-14 10:19:48 +0900

    트래블 높이 수정,링크 수정

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -4,54 +4,55 @@ import { useProductStore } from '../store/useProductStore';
 import "./scss/CategoryPage.scss"
 
 export default function CategoryPage() {
-    const {mainCate, subCate} = useParams();
-    const {items, onFetchItems, mainMenuList, filterItems, onFilterMainCate, onLastCategoryMenu} = useProductStore();
+    const { mainCate, subCate } = useParams();
+    const { items, onFetchItems, mainMenuList, filterItems, onFilterMainCate, onLastCategoryMenu } = useProductStore();
 
     // 한글로 변환
-    const mainCateKo = mainMenuList.find((main)=>main.link===mainCate).name;
-    const subCateKo = mainMenuList.find((main)=>main.link===mainCate).sub?.find((sub)=>sub.link===subCate).name;
-    const miniCate = mainMenuList.find((main)=>main.link===mainCate).sub?.find((sub)=>sub.link===subCate).mini;
+    const mainCateKo = mainMenuList.find((main) => main.link === mainCate).name;
+    const subCateKo = mainMenuList.find((main) => main.link === mainCate).sub?.find((sub) => sub.link === subCate).name;
+    const miniCate = mainMenuList.find((main) => main.link === mainCate).sub?.find((sub) => sub.link === subCate).mini;
 
-    useEffect(()=>{
-        if(items.length === 0){
+    useEffect(() => {
+        if (items.length === 0) {
             onFetchItems();
         }
     }, [items]);
 
-    useEffect(()=>{
-        if(items.length > 0){
+    useEffect(() => {
+        if (items.length > 0) {
             onFilterMainCate(mainCateKo, subCateKo);
             // onLastCategoryMenu();
         }
     }, [items, mainCate, subCate]);
 
-    const onhandleMiniCategory = (mini)=>{
+    const onhandleMiniCategory = (mini) => {
         // console.log("minicateClick");
         onFilterMainCate(mainCateKo, subCateKo, mini);
     }
 
-    console.log("최종 카테",filterItems);
+    console.log("최종 카테", filterItems);
 
-  return (
-    <div className="sub-page-wrap">
-        <h2>{subCateKo}</h2>
-        {miniCate ?         
-            <ul className="mini-menu">
-                {miniCate.map((mini, id)=>
-                    <li key={id} onClick={()=>onhandleMiniCategory(mini)}>
-                        {mini}
+    return (
+        <div className="sub-page-wrap">
+            {/* 슬라이더  */}
+            <h2>{subCateKo}</h2>
+            {miniCate ?
+                <ul className="mini-menu">
+                    {miniCate.map((mini, id) =>
+                        <li key={id} onClick={() => onhandleMiniCategory(mini)}>
+                            {mini}
+                        </li>
+                    )}
+                </ul>
+                : null
+            }
+            <ul className='product-list'>
+                {filterItems.map((item) => (
+                    <li key={item.id}>
+                        {item.productName}
                     </li>
-                )}
+                ))}
             </ul>
-            : null
-        }
-        <ul className='product-list'>
-            {filterItems.map((item)=>(
-                <li key={item.id}>
-                    {item.productName}
-                </li>
-            ))}
-        </ul>
-    </div>
-  )
+        </div>
+    )
 }
