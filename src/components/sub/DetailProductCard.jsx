17f5f233367e03e1a958e29684a4d79d89e0273d
youngseo-claire <claireyslahto@gmail.com>
2026-04-14 18:56:36 +0900

    travel sub title색상변경

--- src/components/sub/DetailProductCard.jsx
+++ src/components/sub/DetailProductCard.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
-export default function DetailProductCard() {
+export default function DetailProductCard({ item }) {
   return (
-    <div>DetailProductCard</div>
+    <div className="product-card">
+      {/* 이미지 */}
+      <div className="card-img">
+        <img src={`/images/category/case/${item.id}_${item.color[0]}_0.jpg`} alt={item.productName} />
+      </div>
+
+      {/* 정보 */}
+      <div className="card-info">
+        <p className="card-name">{item.productName}</p>
+        <p className="card-price">
+          {Number(item.price).toLocaleString()}원
+        </p>
+      </div>
+    </div>
   )
 }
