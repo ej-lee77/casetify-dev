33b236069e05af480483004fef49ac72b96a101b
ej-lee77 <eunji0.0777@gmail.com>
2026-04-14 18:39:47 +0900

    studio, new

--- src/components/main/Studio.jsx
+++ src/components/main/Studio.jsx
@@ -12,12 +12,12 @@ export default function Studio() {
                     <img src="./images/main/studio/studio_pangyo.jpg" alt="판교지점" />
                 </div>
                 <div className="txt-box">
-                    <h3>케이스티파이와 함께 나만의 스타일을 완성하세요</h3>
-                    <ul className="stdio-feature">
-                        <li>나의 취향을 실물로 바로 확인하는 곳, 디자인 및 소개 피팅</li>
-                        <li>기다림 없이 바로 만나는 즐거움, 커스텀 케이스 당일 수령</li>
-                        <li>나만의 개성을 더해줄 포인트, 다양한 테크 악세서리 체험</li>
-                    </ul>
+                    <div className="stdio-feature">
+                        <span className='title'>케이스티파이와 함께 나만의 스타일을 완성하세요</span>
+                        <span>나의 취향을 실물로 바로 확인하는 곳, 디자인 및 소개 피팅</span>
+                        <span>기다림 없이 바로 만나는 즐거움, 커스텀 케이스 당일 수령</span>
+                        <span>나만의 개성을 더해줄 포인트, 다양한 테크 악세서리 체험</span>
+                    </div>
                     <button className="studio-search">매장찾기</button>
                     {/* <Link className="studio-search">매장찾기</Link> */}
                 </div>
