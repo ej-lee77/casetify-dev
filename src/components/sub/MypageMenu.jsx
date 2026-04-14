5afd70451bca9a717a2d9ae20b50374ae7e49fd3
cjw6548 <cjw6548@gmail.com>
2026-04-14 19:46:51 +0900

    260414_07_마이페이지: 왼쪽 메뉴 아이콘 적용 테스트

--- src/components/sub/MypageMenu.jsx
+++ src/components/sub/MypageMenu.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 
 // 마이페이지 메뉴 데이터
 const mypageMenuData = [
-    { id: 1, icon: "./images/icon/icon_favorite.svg", name: "회원정보", tabicon: true, islink: false },
-    { id: 2, icon: "./images/icon/icon_favorite.svg", name: "주문", tabicon: true, islink: false },
+    { id: 1, icon: "./images/icon/mypage_id.svg", name: "회원정보", tabicon: true, islink: false },
+    { id: 2, icon: "./images/icon/btn_shopping-cart.svg", name: "주문", tabicon: true, islink: false },
     { id: 3, icon: "./images/icon/icon_favorite.svg", name: "위시리스트", tabicon: true, islink: false },
     { id: 4, icon: null, name: "케이스티파이 정품 인증", tabicon: false, islink: true },
-    { id: 5, icon: "./images/icon/icon_favorite.svg", name: "기프트 카드", tabicon: true, islink: false },
-    { id: 6, icon: "./images/icon/icon_favorite.svg", name: "로그아웃", tabicon: true, islink: false },
+    { id: 5, icon: "./images/icon/mypage_present.svg", name: "기프트 카드", tabicon: true, islink: false },
+    { id: 6, icon: "./images/icon/mypage_logout.svg", name: "로그아웃", tabicon: true, islink: false },
 ]
 
 export default function MypageMenu() {
