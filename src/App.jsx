06dd1be38d6c47fb1b6385704a55c14678f4313d
cjw6548 <cjw6548@gmail.com>
2026-04-14 12:09:25 +0900

    260414_01_서브페이지:위시리스트 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,24 @@ import Main from './pages/Main'
 import { useEffect } from 'react'
 import { useProductStore } from './store/useProductStore'
 import CategoryPage from './pages/CategoryPage'
+import Mypage from './pages/Mypage'
 
 function App() {
-  const {onFetchItems} = useProductStore();
+  const { onFetchItems } = useProductStore();
 
-  useEffect(()=>{
+  useEffect(() => {
     onFetchItems();
   }, []);
   return (
     <>
-    <Header />
-    <Routes>
-      <Route path='/' element={<Main />}/>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
 
-      <Route path='/:mainCate/:subCate' element={<CategoryPage />}/>
-    </Routes>
-    <Footer />
+        <Route path='/:mainCate/:subCate' element={<CategoryPage />} />
+        <Route path='/mypage' element={<Mypage />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
