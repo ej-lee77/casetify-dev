4509a7b01c02314b9001b038e0807253c24797bd
cky0u0-ctrl <cky0u0@gmail.com>
2026-04-15 19:39:18 +0900

    bp - 캐리어 / cu 이미지 객체 이미지바꿈

--- src/components/main/Custom.jsx
+++ src/components/main/Custom.jsx
@@ -15,7 +15,7 @@ const CUmain = [
                 id: 1,
                 title: "핸드폰 케이스 텍스트 커스터마이징",
                 price: 102000,
-                CUimage: "./images/main/custom/CUProduct05.png"
+                CUimage: "./images/main/custom/CUproduct05.png"
             },
             ,
 
@@ -23,14 +23,14 @@ const CUmain = [
                 id: 2,
                 title: "이어폰 케이스 텍스트 커스터마이징",
                 price: 58000,
-                CUimage: "./images/main/custom/CUProduct06.png"
+                CUimage: "./images/main/custom/CUproduct06.png"
             },
 
             {
                 id: 3,
                 title: "노트북 케이스 텍스트 커스터마이징",
                 price: 83000,
-                CUimage: "./images/main/custom/CUProduct07.png"
+                CUimage: "./images/main/custom/CUproduct07.png"
             },
 
 
@@ -38,7 +38,7 @@ const CUmain = [
                 id: 4,
                 title: "태블릿 케이스 텍스트 커스터마이징",
                 price: 101000,
-                CUimage: "./images/main/custom/CUProduct08.png"
+                CUimage: "./images/main/custom/CUproduct08.png"
             }]
     },
     {
@@ -49,7 +49,7 @@ const CUmain = [
                 id: 5,
                 title: "핸드폰 케이스 포토 커스터마이징",
                 price: 89000,
-                CUimage: "./images/main/custom/CUProduct09.png"
+                CUimage: "./images/main/custom/CUproduct09.png"
             },
             ,
 
@@ -57,14 +57,14 @@ const CUmain = [
                 id: 6,
                 title: "그립 스탠드 포토 커스터마이징",
                 price: 45000,
-                CUimage: "./images/main/custom/CUProduct10.png"
+                CUimage: "./images/main/custom/CUproduct10.png"
             },
 
             {
                 id: 7,
                 title: "카드홀더 스탠드 포토 커스터마이징",
                 price: 51000,
-                CUimage: "./images/main/custom/CUProduct11.png"
+                CUimage: "./images/main/custom/CUproduct11.png"
             },
 
 
@@ -72,7 +72,7 @@ const CUmain = [
                 id: 8,
                 title: "노트북 케이스 포토 커스터마이징",
                 price: 83000,
-                CUimage: "./images/main/custom/CUProduct12.png"
+                CUimage: "./images/main/custom/CUproduct12.png"
             }]
 
     }
