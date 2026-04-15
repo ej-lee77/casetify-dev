6d060d9f36c96da049ae5f3c4fa09b134b4455ff
cjw6548 <cjw6548@gmail.com>
2026-04-15 11:48:13 +0900

    260415_01_마이페이지: 왼쪽 메뉴 호버 효과

--- src/components/sub/MypageMenu.jsx
+++ src/components/sub/MypageMenu.jsx
@@ -1,27 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./scss/MypageMenu.scss"
 import { Link } from 'react-router-dom'
 
 // 마이페이지 메뉴 데이터
 const mypageMenuData = [
     { id: 1, icon: "./images/icon/mypage_id.svg", name: "회원정보", tabicon: true, islink: false },
-    { id: 2, icon: "./images/icon/btn_shopping-cart.svg", name: "주문", tabicon: true, islink: false },
-    { id: 3, icon: "./images/icon/icon_favorite.svg", name: "위시리스트", tabicon: true, islink: false },
+    { id: 2, icon: "./images/icon/mypage_order.svg", name: "주문", tabicon: true, islink: false },
+    { id: 3, icon: "./images/icon/mypage_favorite.svg", name: "위시리스트", tabicon: true, islink: false },
     { id: 4, icon: null, name: "케이스티파이 정품 인증", tabicon: false, islink: true },
     { id: 5, icon: "./images/icon/mypage_present.svg", name: "기프트 카드", tabicon: true, islink: false },
     { id: 6, icon: "./images/icon/mypage_logout.svg", name: "로그아웃", tabicon: true, islink: false },
 ]
 
-export default function MypageMenu() {
+export default function MypageMenu({ sendSelect }) {
+    // hover 상태 파악
+    const [hoverId, setHoverId] = useState(null);
+
     return (
         <>
             <ul className="mypage-menu-list">
                 {mypageMenuData.map((menu) => (
                     <li key={menu.id} className="menu-item">
-                        <button>
-                            {menu.icon && <img src={menu.icon} alt={menu.name} />}
+                        <button
+                            onClick={() => sendSelect(menu)}
+                            onMouseEnter={() => setHoverId(menu.id)}
+                            onMouseLeave={() => setHoverId(null)}
+                        >
+                            {menu.icon &&
+                                <img src={hoverId === menu.id ? menu.icon.replace(".svg", "_white.svg") : menu.icon}
+                                    alt={menu.name} />}
                             <span>{menu.name}</span>
-                            {menu.islink && <span className="link"><img src="./images/icon/external_link.svg" alt="링크이동" /></span>}
+                            {menu.islink && <span className="link"
+                            ><img src={hoverId === menu.id ? "./images/icon/external_link_white.svg" : "./images/icon/external_link.svg"}
+                                alt="링크이동" /></span>}
                         </button>
                     </li>
                 ))}
--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./scss/Mypage.scss"
 import Benefit from '../components/Benefit'
 import WishList from '../components/sub/WishList'
 import MypageMenu from '../components/sub/MypageMenu'
+import { useLocation } from 'react-router-dom'
 
 
 export default function Mypage() {
+  const location = useLocation();
+
+  // 왼쪽 메뉴 체크 변수
+  const [selectMenu, setSelectMenu] = useState()
+
+  // 왼쪽 메뉴를 클릭하면 메뉴 변경
+  const handleMenuClick = ((menu) => {
+    console.log(menu);
+  })
+
   return (
     <div className="sub-page-wrap">
       <div className="inner">
         <div className="my-page-wrap">
           <div className="my-page-menu">
             {/* 마이페이지 메뉴 컴포넌트 */}
-            <MypageMenu />
+            <MypageMenu sendSelect={handleMenuClick} />
           </div>
           <div className="my-page-content">
             {/* 위시리스트 컴포넌트 */}
