708e620480eb2b29f00a3817ea84f63103d8af2a
cjw6548 <cjw6548@gmail.com>
2026-04-15 15:36:25 +0900

    260415_02_마이페이지: 위시리스트 페이지 컴포넌트 추가

--- src/components/sub/WishList.jsx
+++ src/components/sub/WishList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./scss/WishList.scss"
 import MypageTitle from './MypageTitle'
 
 
@@ -6,6 +7,60 @@ export default function WishList() {
     return (
         <div>
             <MypageTitle title={"위시리스트"} />
+            <ul className="wish-list">
+                {/* component item */}
+                <li>
+                    <div className="img-box">
+                        <img src="./images/main/bestproduct/BpProduct001.png" alt="test-img" />
+                        <span className="fav-icon"><img src="./images/icon/icon_favorite_fill.svg" alt="" /> </span>
+                    </div>
+                    <div className="text-box">
+                        <p className="item-name">Happiest KKOTKA in the world</p>
+                        <p className="item-price">₩{Number("29000").toLocaleString()}</p>
+                    </div>
+                </li>
+                {/* 하단 카드아이템 배열 map 적용 후 삭제 */}
+                <li>
+                    <div className="img-box">
+                        <img src="./images/main/bestproduct/BpProduct001.png" alt="test-img" />
+                        <span className="fav-icon"><img src="./images/icon/icon_favorite_fill.svg" alt="" /> </span>
+                    </div>
+                    <div className="text-box">
+                        <p className="item-name">Happiest KKOTKA in the world</p>
+                        <p className="item-price">₩{Number("29000").toLocaleString()}</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="img-box">
+                        <img src="./images/main/bestproduct/BpProduct001.png" alt="test-img" />
+                        <span className="fav-icon"><img src="./images/icon/icon_favorite_fill.svg" alt="" /> </span>
+                    </div>
+                    <div className="text-box">
+                        <p className="item-name">Happiest KKOTKA in the world</p>
+                        <p className="item-price">₩{Number("29000").toLocaleString()}</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="img-box">
+                        <img src="./images/main/bestproduct/BpProduct001.png" alt="test-img" />
+                        <span className="fav-icon"><img src="./images/icon/icon_favorite_fill.svg" alt="" /> </span>
+                    </div>
+                    <div className="text-box">
+                        <p className="item-name">Happiest KKOTKA in the world</p>
+                        <p className="item-price">₩{Number("29000").toLocaleString()}</p>
+                    </div>
+                </li>
+                <li>
+                    <div className="img-box">
+                        <img src="./images/main/bestproduct/BpProduct001.png" alt="test-img" />
+                        <span className="fav-icon"><img src="./images/icon/icon_favorite_fill.svg" alt="" /> </span>
+                    </div>
+                    <div className="text-box">
+                        <p className="item-name">Happiest KKOTKA in the world</p>
+                        <p className="item-price">₩{Number("29000").toLocaleString()}</p>
+                    </div>
+                </li>
+            </ul>
         </div>
     )
 }
