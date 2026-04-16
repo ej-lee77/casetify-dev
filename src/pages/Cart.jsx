e21d1724765934cf238e9a0e890c654effb10972
cjw6548 <cjw6548@gmail.com>
2026-04-16 13:03:23 +0900

    260416_02_마이페이지: 왼쪽 메뉴 아이콘 경로 변경

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,7 +3,7 @@ import "./scss/Cart.scss"
 
 export default function Cart() {
   return (
-    <div className="sub-page-wrap">
+    <div className="sub-page-wrap cart-wrap">
       {/* 페이지 상단 제목 */}
       <div className="inner">
         {/* 상단 제목, 결제 과정 표시 영역 */}
@@ -35,9 +35,18 @@ export default function Cart() {
         {/* 장바구니 상품 리스트 영역 */}
         <div className="cart-wrap">
           <div className="cart-list-area">
+            <div className="cart-title">
+              <input type="checkbox" />
+              <p>상품정보</p>
+              <p>수량</p>
+              <p>가격</p>
+            </div>
             장바구니 리스트
           </div>
           <div className="payment-ctrl-area">
+            <div className="cart-title">
+              <p>주문 예상 금액</p>
+            </div>
             금액영역(주문금액,상품주문 버튼)
           </div>
         </div>
