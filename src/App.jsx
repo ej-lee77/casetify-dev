ed316f617405cae90aa318d2a8bc209e7ab9fd75
ej-lee77 <eunji0.0777@gmail.com>
2026-04-17 18:40:07 +0900

    login

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import CategoryPagePractice from './pages/CategoryPagePractice'
 import Join from './pages/Join'
 import NaverCallBack from './pages/NaverCallBack'
 import JoinMail from './pages/JoinMail'
+import LoginFindId from './pages/LoginFindId'
 
 function App() {
   const { onFetchItems } = useProductStore();
@@ -37,6 +38,7 @@ function App() {
 
         <Route path='/login' element={<Login />} />
         <Route path='/login/naver' element={<NaverCallBack />} />
+        <Route path='/login/find/:content' element={<LoginFindId />} />
         <Route path='/join' element={<Join />} />
         <Route path='/join/mail' element={<JoinMail />} />
         <Route path='/mypage' element={<Mypage />} />
