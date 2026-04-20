33da490d9354597bfd528706b587e13b9da3cb7b
ej-lee77 <eunji0.0777@gmail.com>
2026-04-21 00:19:56 +0900

    header userMenu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -90,8 +90,22 @@ export default function Header() {
           </li>
           {user ? (
             <>
-            <li>
-              <Link to="/mypage" className='mypage-icon'><img src="/images/icon/user.svg" alt="내정보" /></Link>
+            <li onMouseEnter={() => setMenuActive("gnb")} onMouseLeave={() => setMenuActive(null)}>
+              <Link className='mypage-icon'><img src="/images/icon/user.svg" alt="내정보" /></Link>
+              <ul className={`user-menu ${MenuActive === "gnb" ? "active" : "" }`}>
+                <li>
+                  <Link to="/mypage" state={{ menu: "회원정보" }}>회원정보</Link>
+                </li>
+                <li>
+                  <Link to="/mypage" state={{ menu: "주문" }}>주문</Link>
+                </li>
+                <li>
+                  <Link to="/mypage" state={{ menu: "케이스티파이 정품 인증" }}>정품인증</Link>
+                </li>
+                <li>
+                  <Link to="/mypage" state={{ menu: "기프트 카드" }}>기프트카드</Link>
+                </li>
+              </ul>
             </li>
             <li>
               <a className='logout-icon' onClick={handleLogout}><img src="/images/icon/logout.svg" alt="로그아웃" /></a>
