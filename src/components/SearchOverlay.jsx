a1f122ae720cc695e0cf903209f36197032984d8
cjw6548 <cjw6548@gmail.com>
2026-04-20 19:33:40 +0900

    260420_02_검색: input 레이아웃 설계

--- src/components/SearchOverlay.jsx
+++ src/components/SearchOverlay.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import "./scss/SearchOverlay.scss";
+import { Link } from 'react-router-dom';
 
 export default function SearchOverlay({ isActive, onClose }) {
     return (
         <div className={`search-overlay-wrap ${isActive ? "active" : ""}`}>
-            (temp - 검색 오버레이)
+            <div className="search-content-outer-wrap">
+                <h1 className="logo">
+                    <Link to="/"><img src="/images/casetify-logo-15th.png" alt="casetify" /></Link>
+                </h1>
+                <div className="search-center-wrap">
+                    <div className="search-content-inner-wrap">
+                        <div className="search-bar">
+                            <label>
+                                <input type="text" placeholder="궁금한 내용의 단어나 키워드로 검색하세요" />
+                            </label>
+                            <button className="btn-search">
+                                <img src="/images/icon/search_var.svg" alt="" />
+                            </button>
+                            <button className="btn-reset">초기화</button>
+                        </div>
+                        <div className="recent-search-wrap">최근 검색어</div>
+                        <div className="fav-search-wrap">인기 검색어</div>
+                        <div className="recommend-wrap">추천 상품</div>
+                    </div>
+                    <div className="img-search">이미지 검색</div>
+                </div>
+            </div>
             {/* 검색 닫기 */}
-            <p onClick={onClose}><img src="/images/icon/close.svg" alt="검색닫기" /></p>
+            <p className="close-btn" onClick={onClose}><img src="/images/icon/close.svg" alt="검색닫기" /></p>
         </div>
     )
 }
