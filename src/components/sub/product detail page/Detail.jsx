84783aef5d077d471c36e09bdc007c862783bdbc
youngseo-claire <claireyslahto@gmail.com>
2026-04-29 16:42:40 +0900

    상품마다디테일변경,장바구니,찜하기,상품카드 디자인변경,select ul>li로 변경

--- src/components/sub/product detail page/Detail.jsx
+++ src/components/sub/product detail page/Detail.jsx
@@ -2,24 +2,42 @@ import React from 'react'
 import Benefit from '../../Benefit'
 import "./scss/detail.scss"
 
+const detailImages = {
+    phone: [
+        "/images/KY/productdetail/detail/case-detail.png",
+    ],
+    earbuds: [                          // ← "earphone" → "earbuds"
+        "/images/KY/productdetail/detail/earphone-detail.png",
+    ],
+    laptop: [
+        "/images/KY/productdetail/detail/laptop-detail.png",
+    ],
+    tablet: [                           // ← "ipad" → "tablet"
+        "/images/KY/productdetail/detail/ipad-detail.png",
+    ],
+    etc: [
+        "/images/KY/productdetail/detail/etc-detail.png",
+    ],
+    travel: [
+        "/images/KY/productdetail/detail/travel-detail.png",
+    ],
+    watch: [
+        "/images/KY/productdetail/detail/watch-detail.png",
+    ],
+}
 
+export default function Detail({ item }) {
+    const target = item?.productTarget || "phone"
+    const images = detailImages[target] || detailImages["phone"]
 
-
-
-export default function Detail() {
     return (
         <div className="detail">
             <div className="img-box">
- <img src="/images/KY/productdetail/detail/image257.png" alt="" />
-
-<img src="/images/KY/productdetail/detail/image256.png" alt="" />
-<img src="/images/KY/productdetail/detail/image253.png" alt="" />
-
+                {images.map((src, idx) => (
+                    <img key={idx} src={src} alt={`상세이미지 ${idx + 1}`} />
+                ))}
             </div>
-
             <Benefit />
-
         </div>
     )
 }
-
