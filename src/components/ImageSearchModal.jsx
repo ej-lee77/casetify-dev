4ee83dead449aa746ba9566cb5251f2b4f2ab315
ej-lee77 <eunji0.0777@gmail.com>
2026-04-30 18:47:25 +0900

    검색

--- src/components/ImageSearchModal.jsx
+++ src/components/ImageSearchModal.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./scss/ImageSearchModal.scss"
+import { useProductStore } from '../store/useProductStore';
+import { useNavigate } from 'react-router-dom';
 
 export default function ImageSearchModal({ modalCheck, setModalCheck }) {
+    const [isDragging, setIsDragging] = useState(false);
+    const { onSetSearchWord } = useProductStore();
+    const navigate = useNavigate();
+    
+    const handleFile = (file) => {
+        if (file && file.type.startsWith("image/")) {
+            onSetSearchWord("폰");
+            // 검색어 페이지로 이동
+            navigate('/search');
+            setModalCheck(false);
+        } else {
+            alert("이미지 파일만 업로드 가능합니다.");
+        }
+    };
+
+    const handleFileChange = (e) => {
+        handleFile(e.target.files[0]);
+    };
+
+    const onDragOver = (e) => {
+        e.preventDefault();
+        setIsDragging(true);
+    };
+    const onDragLeave = () => setIsDragging(false);
+    const onDrop = (e) => {
+        e.preventDefault();
+        setIsDragging(false);
+        handleFile(e.dataTransfer.files[0]);
+    };
     return (
         <div className={`img-search-modal-overlay ${modalCheck ? "active" : ""}`}>
             {/* 이미지 검색 모달 검정 배경 */}
@@ -18,18 +49,40 @@ export default function ImageSearchModal({ modalCheck, setModalCheck }) {
                         </div>
                     </div>
                     <div className="img-search-modal-content">
-                        <div className="dropbox-wrap">
-                            <div className="upload-btn-text-wrap">
-                                <p>이미지를 여기로 드래그하거나 파일 업로드</p>
-                                <div className="plus-btn">
-                                    <img src="./images/icon/btn_plus.svg" alt="파일 추가" />
+                        <div className={`dropbox-wrap ${isDragging ? "dragging" : ""}`}
+                            onDragOver={onDragOver}
+                            onDragLeave={onDragLeave}
+                            onDrop={onDrop}>
+                            <label htmlFor="file-upload">
+                                <div className="upload-btn-text-wrap">
+                                    
+                                    <p>이미지를 여기로 드래그하거나 파일 업로드</p>
+                                    <div className="plus-btn">
+                                        <img src="./images/icon/btn_plus.svg" alt="파일 추가" />
+                                    </div>
                                 </div>
-                            </div>
+                            </label>
                             {/* file upload input */}
+                            <input 
+                                type="file" 
+                                id="file-upload" 
+                                style={{ display: 'none' }} 
+                                accept="image/*"
+                                onChange={handleFileChange}
+                            />
                         </div>
                     </div>
                 </div>
             </div>
+            {previewSrc && (
+                <img 
+                    src={previewSrc} 
+                    ref={hiddenImgRef} 
+                    alt="hidden-preview" 
+                    style={{ display: 'none' }} 
+                    onLoad={(e) => runPrediction(e.target)} 
+                />
+            )}
         </div>
     )
 }
