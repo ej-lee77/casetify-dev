2817dac5195bc570dea2a16d36b3b86f4e37a120
cjw6548 <cjw6548@gmail.com>
2026-05-02 15:48:25 +0900

    260502_매장찾기: 매장 정보 팝업 자유로운 이동 구현

--- src/components/MapPopup.jsx
+++ src/components/MapPopup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useMapStore } from '../store/useMapStore';
 import "./scss/MapPopup.scss"
 
@@ -7,8 +7,76 @@ export default function MapPopup({ onClose }) {
 
   if (!selectedLocation) return null;
 
+  const popupRef = useRef(null);
+
+  // 매장 정보 팝업 위치 상태관리
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isDrag, setIsDrag] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+  // 초기 중앙 위치 계산
+  useEffect(() => {
+    if (!popupRef.current) return;
+
+    const rect = popupRef.current.getBoundingClientRect();
+
+    //  set position
+    setPosition({
+      x: window.innerWidth / 2 - rect.width / 2,
+      y: window.innerHeight / 2 - rect.height / 2 - 260
+    });
+  }, []);
+
+  // 드래그 이동
+  useEffect(() => {
+    const move = (e) => {
+      if (!isDrag) return;
+
+      const popupWidth = popupRef.current.offsetWidth;
+      const popupHeight = popupRef.current.offsetHeight;
+
+      let newX = e.clientX - offset.x;
+      let newY = e.clientY - offset.y;
+
+      // 화면 범위 제한
+      const maxX = window.innerWidth - popupWidth;
+      const maxY = window.innerHeight - popupHeight;
+
+      newX = Math.max(16, Math.min(newX, maxX - 16));
+      newY = Math.max(16, Math.min(newY, maxY));
+
+      setPosition({
+        x: newX,
+        y: newY
+      });
+    };
+    const up = () => setIsDrag(false);
+    window.addEventListener("mousemove", move);
+    window.addEventListener("mouseup", up);
+
+    return () => {
+      window.removeEventListener("mousemove", move);
+      window.removeEventListener("mouseup", up);
+    };
+
+  }, [isDrag, offset])
+
+  // 마우스 이벤트 - 드래그 시작
+  const handleMouseDown = (e) => {
+    setIsDrag(true);
+
+    setOffset({
+      x: e.clientX - position.x,
+      y: e.clientY - position.y
+    });
+  };
+
   return (
-    <div className='MapPopup'>
+    <div
+      ref={popupRef}
+      className='MapPopup'
+      style={{ top: position.y, left: position.x }}
+    >
       {/* <div className="map-popup-title">
         <p>매장 상세정보</p>
         <button onClick={onClose}>
@@ -20,8 +88,9 @@ export default function MapPopup({ onClose }) {
         <img src="../images/icon/close-24dp.svg" alt="닫기" />
       </button>
 
-      <h2>{selectedLocation.name}</h2>
-
+      <div className="drag-area" onMouseDown={handleMouseDown}>
+        <h2>{selectedLocation.name}</h2>
+      </div>
       <div className="store-img-area">
         <img src={selectedLocation.img} alt="" />
       </div>
